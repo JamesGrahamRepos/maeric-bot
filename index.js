fe929f13b6c2ebf2a7e69b150677dec3029605b1
JamesGrahamRepos <63750324+JamesGrahamRepos@users.noreply.github.com>
2020-04-15 16:29:39 -0400

    Add files via upload
    Researching how Discord bots work

---
+++ index.js
@@ -0,0 +1,30 @@
+require('dotenv').config()
+const Discord = require('discord.js')
+const client = new Discord.Client()
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`)
+})
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!')
+  }
+})
+
+client.on('guildMemberAdd', member => {
+  member.send(
+    'Hello, this is from Juniper! Mwhwhahaha'
+  )
+})
+
+// Create an event listener for messages
+client.on('message', message => {
+  // If the message is "what is my avatar"
+  if (message.content === 'what is my avatar') {
+    // Send the user's avatar URL
+    message.reply(message.author.avatarURL);
+  }
+})
+
+client.login(process.env.BOT_TOKEN)
